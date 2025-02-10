898e5640219d3caea5d3b7330ae84a260f9a3158
developperjun <f1121069@kgu.jp>
2025-02-10 18:03:48 +0900

    003_SetupDBwithDocker&Django

--- defaultmyapp/models.py
+++ defaultmyapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
