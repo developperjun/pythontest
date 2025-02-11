60443914a45fca5a3eacecdd9008ced9410e585d
developperjun <f1121069@kgu.jp>
2025-02-11 11:45:52 +0900

    004_UsetheDjangoAdmin

--- defaultmyapp/admin.py
+++ defaultmyapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category
+
+@admin.register(Category)  # 管理画面に登録
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # 管理画面で表示する列
